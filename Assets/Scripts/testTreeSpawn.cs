288775fc90fd05124b2ea7c103495ef531b29997
Matthew Turney <mturn006@ucr.edu>
2014-04-14 19:54:54 -0700

    worked on treespawning.

--- Assets/Scripts/testTreeSpawn.cs
+++ Assets/Scripts/testTreeSpawn.cs
@@ -4,17 +4,26 @@ using System.Collections;
 public class testTreeSpawn : MonoBehaviour {
 
 	public Transform treeSection;
-	public float distance = 20.0f; //This is the distance from each tree to the spawner
+	public float distance = 15.0f; //This is the distance from each tree to the spawner.
+	public float treeSectionHeight = 8.0f;
+	public Transform [] traps;
+	
 
 	private float spawnAngle = 120.0f;
+	private float angle1 = 120.0f * Mathf.PI / 180;
+	private float angle2 = 240.0f * Mathf.PI / 180;
+
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 1; i++) 
+
+		float height = 8.0f; //TODO how can I access the height of the trunk?
+
+		for (int i = 0; i < 20; i++) 
 		{
-			Instantiate(treeSection, new Vector3(distance, 2, 0), Quaternion.identity);
-			Instantiate(treeSection, new Vector3(distance * Mathf.Cos(.75f), 2, distance * Mathf.Sin (.75f)), Quaternion.identity);
-			Instantiate(treeSection, new Vector3(distance * Mathf.Cos(-.25f), 2, distance * Mathf.Sin (-.250f)), Quaternion.identity);
+			Instantiate(treeSection, new Vector3(distance,height*i, 0), Quaternion.identity);
+			Instantiate(treeSection, new Vector3(distance * Mathf.Cos(angle1),height*i, distance * Mathf.Sin (angle1)), Quaternion.identity);
+			Instantiate(treeSection, new Vector3(distance * Mathf.Cos(angle2),height*i, distance * Mathf.Sin (angle2)), Quaternion.identity);
 
 	
 		}
@@ -24,4 +33,21 @@ public class testTreeSpawn : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void spawnTrees () {
+		float height = 8.0f; //TODO how can I access the height of the trunk?
+		
+		for (int i = 0; i < 20; i++) 
+		{
+			Instantiate(treeSection, new Vector3(distance,height*i, 0), Quaternion.identity);
+			Instantiate(treeSection, new Vector3(distance * Mathf.Cos(angle1),height*i, distance * Mathf.Sin (angle1)), Quaternion.identity);
+			Instantiate(treeSection, new Vector3(distance * Mathf.Cos(angle2),height*i, distance * Mathf.Sin (angle2)), Quaternion.identity);
+				
+		}
+	}
+
+	void spawnTraps () {
+
+	}
+
 }
