923be9549fe6ddd4cc9e226a29eea512f351adc7
Justin Cano <jcano001@ucr.edu>
2014-04-16 23:20:23 -0700

    added tree trunk destroyer script to tree trunk prefab

---
+++ Assets/Scripts/DestroyTreeTrunk.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTreeTrunk : MonoBehaviour {
+
+	public float offset = 20.0f;
+
+	public GameObject mainCam;
+	public GameObject thisTrunk;
+
+	// Use this for initialization
+	void Start () {
+		mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+		thisTrunk = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform parent = thisTrunk.transform.parent;
+		if(mainCam.transform.position.y >= thisTrunk.transform.position.y+offset && parent.tag == "TreeSpawner") {
+			Transform child = thisTrunk.transform.GetChild(0);
+			child.transform.parent = parent;
+			Destroy(thisTrunk);
+		}
+	}
+}
