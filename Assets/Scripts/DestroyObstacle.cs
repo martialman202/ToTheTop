72f355fab1c0bde2cf8bc9fa1c68441ac6879197
Matthew Turney <mturn006@ucr.edu>
2014-04-21 11:59:58 -0700

    trying to pull from origin

---
+++ Assets/Scripts/DestroyObstacle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObstacle : MonoBehaviour {
+
+	public float offset = 20.0f;
+	
+	public GameObject mainCam;
+
+	// Use this for initialization
+	void Start () {
+		mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mainCam.transform.position.y >= this.gameObject.transform.position.y + offset)
+			Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+	private GameObject player ;
+	private testAutoMonkey monkeyScript = player.GetComponent<testAutoMonkey>();
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		monkeyScript = player.GetComponent<testAutoMonkey>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (110, 110, 200, 130), "Life: " + Other.lifePoints);
+	}
+}
