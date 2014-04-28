24fb03e789437ac63b2e41e6da9c3f23e8fcf86c
Justin Cano <jcano001@ucr.edu>
2014-04-28 14:49:36 -0700

    added connie's CollisionWObstacle script

---
+++ Assets/Scripts/CollisionWObstacle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionWObstacle : MonoBehaviour {
+
+	private Color origColor;
+	private int lastHit = 15;
+
+	// Use this for initialization
+	void Start () {
+		origColor = gameObject.renderer.material.color;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Obstacle") {
+			print("Hit obstacle, should change color");
+			lastHit = 0;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(lastHit < 15) {
+			gameObject.renderer.material.color = Color.red;
+			lastHit++;
+		}
+		else {
+			gameObject.renderer.material.color = origColor;
+		}
+	} 
+}
