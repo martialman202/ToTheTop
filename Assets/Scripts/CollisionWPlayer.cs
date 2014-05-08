87e8d7aa93514d55226680cb1ea1402b8577a8b1
Justin Cano <jcano001@ucr.edu>
2014-05-07 19:17:06 -0700

    snake attacks upon collision with player

--- Assets/Scripts/CollisionWPlayer.cs
+++ Assets/Scripts/CollisionWPlayer.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class CollisionWPlayer : MonoBehaviour {
 
+	Animation animation;
+
+	void Start() {
+		animation = this.GetComponent<Animation> ();
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player") {
 			//Debug.Break ();
 			//Application.LoadLevel (Application.loadedLevelName); //Change scene
+			if (animation != null) animation.Play("attack");
 		}
 	}
 }
