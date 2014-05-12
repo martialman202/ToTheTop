6cceec5eaa63f41cbf400b28eee7086a29eaf93c
Justin Cano <jcano001@ucr.edu>
2014-05-12 14:27:47 -0700

    added exit button

--- Assets/Scripts/TitleSceneGUI.cs
+++ Assets/Scripts/TitleSceneGUI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TitleSceneGUI : MonoBehaviour {
 	void OnGUI() {
 		// Make a group on the center of the screen
-		GUI.BeginGroup (new Rect (Screen.width / 2 - 75, Screen.height / 2 - 75, 150, 150));
+		GUI.BeginGroup (new Rect (Screen.width / 2 - 75, Screen.height / 2 - 75, 150, 210));
 			// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
 			
 			// We'll make a box so you can see where the group is on-screen.
@@ -12,6 +12,8 @@ public class TitleSceneGUI : MonoBehaviour {
 				Application.LoadLevel ("MainScene");
 			if (GUI.Button (new Rect (0, 70, 150, 50), "Arcade"))
 				Application.LoadLevel ("DifficultySelectionScene");
+			if (GUI.Button (new Rect (0, 140, 150, 50), "Exit"))
+				Application.Quit ();
 			
 		// End the group we started above. This is very important to remember!
 		GUI.EndGroup ();
