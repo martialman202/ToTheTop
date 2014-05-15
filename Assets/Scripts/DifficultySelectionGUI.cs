00a331e3fe5fb6559ac3f5b03841fd6824651f10
Connie Nguyen <knguy068@ucr.edu>
2014-05-14 18:36:30 -0700

    Changed menu button sizes. Could move variables to a singleton in future

--- Assets/Scripts/DifficultySelectionGUI.cs
+++ Assets/Scripts/DifficultySelectionGUI.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class DifficultySelectionGUI : MonoBehaviour {
 
-	private float buttonHeight = 50.0f;
+	private float buttonHeight = Screen.width/6;
+	private float betweenButton = Screen.width/4;
 
 	void OnGUI() {
 		// Make a group on the center of the screen
-		GUI.BeginGroup (new Rect (Screen.width / 2 - Screen.width/4, Screen.height / 2 - Screen.width/4, Screen.width/2, 150));
+		GUI.BeginGroup (new Rect (Screen.width / 2 - Screen.width/4, Screen.height / 2 - Screen.width/4, Screen.width/2, buttonHeight * 3));
 		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
 		
 		// We'll make a box so you can see where the group is on-screen.
 		if (GUI.Button (new Rect (0, 0, Screen.width/2, buttonHeight), "Easy"))
 			Application.LoadLevel ("EasyLevels");
-		GUI.Button (new Rect (0, 75, Screen.width/2, buttonHeight), "Medium");
+		GUI.Button (new Rect (0, betweenButton, Screen.width/2, buttonHeight), "Medium");
 			//A/plication.LoadLevel ("DifficultySelectionScene");
 		//GUI.Button (new Rect (0, 140, 150, 50), "Hard");
 		
--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class GameOverScript : MonoBehaviour {
 
-	private float buttonHeight = 50.0f;
+	private float buttonHeight = Screen.width/6;
+	private float betweenButton = Screen.width/4;
 
 	// Use this for initialization
 	void Start () {
@@ -11,13 +12,13 @@ public class GameOverScript : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		GUI.BeginGroup (new Rect (Screen.width/2 - Screen.width/4, Screen.height/2 - Screen.width/4, Screen.width/2, 300));
+		GUI.BeginGroup (new Rect (Screen.width/2 - Screen.width/4, Screen.height/2 - Screen.width/4, Screen.width/2, buttonHeight*4));
 			GUI.Label(new Rect(Screen.width/8, 0, Screen.width/2, 30), "GAME OVER");
-			if(GUI.Button(new Rect(0, 50, Screen.width/2, buttonHeight), "Play again?"))
+			if(GUI.Button(new Rect(0, 0.5f*betweenButton, Screen.width/2, buttonHeight), "Play again?"))
 			{
 				Application.LoadLevel(PlayerPrefs.GetInt("previousLevel"));
 			}
-			if(GUI.Button(new Rect(0, 125, Screen.width/2, buttonHeight), "Main Menu"))
+			if(GUI.Button(new Rect(0, 1.5f*betweenButton, Screen.width/2, buttonHeight), "Main Menu"))
 			{
 				Application.LoadLevel("TitleScene");
 			}
