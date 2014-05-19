406c5a25c1adbfc679673b151973f52b151042c0
Connie Nguyen <knguy068@ucr.edu>
2014-05-19 13:42:03 -0700

    Grid level selector framework set up

--- Assets/Scripts/DifficultySelectionGUI.cs
+++ Assets/Scripts/DifficultySelectionGUI.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class DifficultySelectionGUI : MonoBehaviour {
 
 	public GUISkin menuSkin;
+	public Texture2D [] stars = new Texture2D[4];
 	private float buttonHeight = Screen.width/6;
 	private float betweenButton = Screen.width/4;
 
@@ -12,8 +13,10 @@ public class DifficultySelectionGUI : MonoBehaviour {
 			Application.LoadLevel("TitleScene");
 		}
 
-		// Make a group on the center of the screen
 		GUI.skin = menuSkin;
+
+		// Make a group on the center of the screen
+		/*
 		GUI.BeginGroup (new Rect (Screen.width / 2 - Screen.width/4, Screen.height / 2 - Screen.width/4, Screen.width/2, buttonHeight * 3));
 		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
 		
@@ -25,6 +28,21 @@ public class DifficultySelectionGUI : MonoBehaviour {
 		//GUI.Button (new Rect (0, 140, 150, 50), "Hard");
 		
 		// End the group we started above. This is very important to remember!
+		GUI.EndGroup ();
+		*/
+
+		// Grid level selection
+		GUI.BeginGroup(new Rect(0.1f * Screen.width, Screen.height/2 - Screen.width/4, 0.9f * Screen.width, 3 * buttonHeight));
+		//Star labels
+		GUI.Label(new Rect(0.02f * Screen.width, 0, 0.22f * Screen.width, 0.1f * Screen.width), stars[1]);
+		GUI.Label(new Rect(0.31f * Screen.width, 0, 0.22f * Screen.width, 0.1f * Screen.width), stars[2]);
+		GUI.Label(new Rect(0.60f * Screen.width, 0, 0.22f * Screen.width, 0.1f * Screen.width), stars[3]);
+
+		//Level buttons
+		if(GUI.Button(new Rect(0, 0.1f * Screen.width, 0.22f * Screen.width, 0.22f * Screen.width), "1"))
+			Application.LoadLevel("EasyLevels");
+		if(GUI.Button(new Rect(0.29f * Screen.width, 0.1f * Screen.width, 0.22f * Screen.width, 0.22f * Screen.width), "2"));
+		if(GUI.Button(new Rect(0.58f * Screen.width, 0.1f * Screen.width, 0.22f * Screen.width, 0.22f * Screen.width), "3"));
 		GUI.EndGroup ();
 	}
 }
