195270650808f72ef9241ad07a8f26035ea49713
Justin Cano <jcano001@ucr.edu>
2014-05-21 19:31:25 -0700

    added coconut prefab and script

---
+++ Assets/Scripts/Coconut.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coconut : MonoBehaviour {
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (Vector3.down * speed * Time.deltaTime);
+	}
+}
