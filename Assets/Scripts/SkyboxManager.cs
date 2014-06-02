2acd6906174a80be699287d6957a320a6bd381b3
Connie Nguyen <knguy068@ucr.edu>
2014-06-02 15:31:23 -0700

    Added new custom skybox

---
+++ Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxManager : MonoBehaviour {
+
+	public Skybox [] skyboxes = new Skybox[2];
+
+	// Use this for initialization
+	void Start () {
+		Skybox = skyboxes[0];
+		if(Manager.Instance.levelIndex > 3)
+			Skybox = skyboxes[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
