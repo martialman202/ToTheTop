cde9f571d951fa3c4d0c07729688e351bae1e94b
Connie Nguyen <knguy068@ucr.edu>
2014-06-02 16:07:14 -0700

    Added night skybox

--- Assets/Scripts/SkyboxManager.cs
+++ Assets/Scripts/SkyboxManager.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class SkyboxManager : MonoBehaviour {
 
-	public Material [] skyboxes = new Material[2];
+	public Material [] skyboxes = new Material[3];
 
 	// Use this for initialization
 	void Start () {
 		Skybox sky = (Skybox)gameObject.GetComponent("Skybox");
-		if((Manager.Instance.levelIndex + 1) > 3)
+		if((Manager.Instance.levelIndex > 2) && (Manager.Instance.levelIndex <= 5))
 			sky.material = skyboxes[1];
+		else if(Manager.Instance.levelIndex > 5)
+			sky.material = skyboxes[2];
 	}
 }
